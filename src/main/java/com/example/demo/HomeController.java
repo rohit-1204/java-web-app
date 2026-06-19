6e7d4b0090bcca9f7917ba23ce41f8cca124a349
Rohit <rohitpathare1204@gmail.com.com>
2026-06-19 21:19:02 +0530

    initial

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "Hello Rohit Pathare! Welcome to Java Web App.";
+    }
+}
